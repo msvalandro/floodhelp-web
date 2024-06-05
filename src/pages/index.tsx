d4c7af2b033570135d152c01fbe87d05a82f4f22
msvalandro <msvalandro@gmail.com>
2024-06-05 21:17:40 +0100

    create initial setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home | FloodHelp</title>
+      </Head>
+
+      <h1>hello world.</h1>
+    </>
+  )
+}
